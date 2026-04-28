14abb91c8df1b36908df94dfee614d40a67d4cc9
wan <zakwanzakki310806@gmail.com>
2026-04-28 18:23:49 +0700

    update models

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([
+    'name',
+    'email',
+    'password',
+    'username',
+    'pekerjaan',
+    'role',
+    'theme_preference'
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
