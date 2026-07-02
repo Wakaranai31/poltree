4f477d16a7a4ba6c6c4943a07a51ce8a6f2962aa
wan <zakwanzakki310806@gmail.com>
2026-07-02 11:22:47 +0700

    add simple program to run system checker

---
+++ RunWeb.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set GIT_BASH="C:\Program Files\Git\git-bash.exe"
+
+:: Jendela 1: Menjalankan Node.js server (Vite/Mix)
+start "" %GIT_BASH% -c "echo 'Menjalankan: npm run dev'; npm run dev; exec bash"
+
+:: Jendela 2: Menjalankan Laravel Reverb (WebSocket)
+start "" %GIT_BASH% -c "echo 'Menjalankan: php artisan reverb:start'; php artisan reverb:start; exec bash"
+
+:: Jendela 3: Menjalankan Queue Worker
+start "" %GIT_BASH% -c "echo 'Menjalankan: php artisan queue:work'; php artisan queue:work; exec bash"
+
+:: Jendela 4: Menjalankan Schedule Worker
+start "" %GIT_BASH% -c "echo 'Menjalankan: php artisan schedule:work'; php artisan schedule:work; exec bash"
+
+exit
